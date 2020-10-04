08976f822568bdf38860acaec2e15db1f1cf18a4
Tural <tural_5411@mail.ru>
2020-10-04 19:06:09 +0400

    algoritma

---
+++ BaseAlgoritms/Program.cs
@@ -0,0 +1,492 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAlgoritms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(test1(1, 2)); //1
+            //Console.WriteLine(test1(3, 2));
+            //Console.WriteLine(test1(2, 2));
+
+            //Console.WriteLine(test2(53)); //2
+            //Console.WriteLine(test2(30));
+            //Console.WriteLine(test2(51));
+
+            //Console.WriteLine(test3(30, 0)); //3
+            //Console.WriteLine(test3(25, 5));
+            //Console.WriteLine(test3(10, 20));
+            //Console.WriteLine(test3(20, 25));
+
+            //Console.WriteLine(test4(103)); //4
+            //Console.WriteLine(test4(90));
+            //Console.WriteLine(test4(89));
+
+            //Console.WriteLine(test5("if else")); //5 
+            //Console.WriteLine(test5("else"));
+
+            //Console.WriteLine(test6("Pyhton", 1)); //6
+            //Console.WriteLine(test6("Pyhton", 0));
+            //Console.WriteLine(test6("Pyhton", 4));
+
+            //Console.WriteLine(test7("abcd"));  //7 *****
+            //Console.WriteLine(test7("a"));
+            //Console.WriteLine(test7("xy"));
+
+            //Console.WriteLine(test8("C Sharp"));  //8
+            //Console.WriteLine(test8("JS"));
+            //Console.WriteLine(test8("a"));
+
+            //Console.WriteLine(test9("Red"));  //9
+            //Console.WriteLine(test9("Green"));
+            //Console.WriteLine(test9("1"));
+
+            //Console.WriteLine(test10(3));  //10
+            //Console.WriteLine(test10(14));
+            //Console.WriteLine(test10(12));
+            //Console.WriteLine(test10(37));
+
+
+            ////Console.WriteLine(test12("C#"));   //11
+            ////Console.WriteLine(test12("C# .MVC"));
+            ////Console.WriteLine(test12("java"));
+
+            //Console.WriteLine(test12(120, -1));  //12
+            //Console.WriteLine(test12(-1, 120));
+            //Console.WriteLine(test12(2, 120));
+
+            //Console.WriteLine(test13(100, 199));  //13
+            //Console.WriteLine(test13(250, 300));
+            //Console.WriteLine(test13(105, 190));
+
+            //Console.WriteLine(test14(11, 20, 12));  //14
+            //Console.WriteLine(test14(30, 30, 20));
+            //Console.WriteLine(test14(15, 12, 8));
+
+            //Console.WriteLine(test15("Pyhton"));  //15
+            //Console.WriteLine(test15("ytade"));
+            //Console.WriteLine(test15("jsues"));
+
+            //Console.WriteLine(test16(1, 2, 3)); //16
+            //Console.WriteLine(test16(1, 3, 2));
+            //Console.WriteLine(test16(1, 1, 1));
+            //Console.WriteLine(test16(1, 2, 2));
+
+            //Console.WriteLine(test17(78, 95)); //17
+            //Console.WriteLine(test17(95, 95));
+            //Console.WriteLine(test17(99, 70));
+
+
+
+            //Console.WriteLine(test18(78, 95)); //18
+            //Console.WriteLine(test18(25, 35));
+            //Console.WriteLine(test18(40, 50));
+            //Console.WriteLine(test18(55, 60));
+
+            //Console.WriteLine(test19(78, 95)); //19
+            //Console.WriteLine(test19(20, 30));
+            //Console.WriteLine(test19(21, 25));
+            //Console.WriteLine(test19(28, 28));
+
+            //Console.WriteLine(test20("frizz")); //20
+            //Console.WriteLine(test20("zane"));
+            //Console.WriteLine(test20("Zazz"));
+            //Console.WriteLine(test20("false"));
+
+            //Console.WriteLine(test21(123,456)); //21
+            //Console.WriteLine(test21(15,125));
+            //Console.WriteLine(test21(7,87));
+            //Console.WriteLine(test21(12,45));
+
+            //Console.WriteLine(test22("Phtyon")); //22
+            //Console.WriteLine(test22("js"));
+            //Console.WriteLine(test22("Javascript"));
+            //Console.WriteLine(test22("PHP"));
+
+            //Console.WriteLine(test23("js",5)); //23
+            //Console.WriteLine(test23("C",3));
+            //Console.WriteLine(test23("PHP",2));
+
+            //Console.WriteLine(test24("Phyton", 5)); //24
+            //Console.WriteLine(test24("JS", 3));
+            //Console.WriteLine(test24("Pyhton", 2));
+
+            //Console.WriteLine(test25("bbaaccaag"));
+            //Console.WriteLine(test25("jkiaaasew"));
+            //Console.WriteLine(test25("JSaaakoiaa"));
+
+
+            //int say1; *******************
+            //int say2;
+            //int toplam = 0;
+
+
+            //say1 =Convert.ToInt32( Console.ReadLine());
+            //say2 =Convert.ToInt32( Console.ReadLine());
+
+            //for (int i = 0; i < say2; i++)
+            //{
+            //    toplam = say1 + toplam;
+            //}
+            //Console.WriteLine(toplam);*************
+
+            //********************
+            //int say;
+            //int toplam = 0;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    say = Convert.ToInt32(Console.ReadLine());
+            //    if (say>=10 && say <= 20)
+            //    {
+            //        toplam += say;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("10 ve 20 araliqinda olmalidir");
+            //    }
+            //}
+            //Console.WriteLine(toplam);
+            //********************
+
+            //int say;
+            //int eded=0;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("1-10 arasi reqem girin");
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.ResetColor();
+            //    say = Convert.ToInt32(Console.ReadLine());
+            //    if (say != null)
+            //    {
+            //        if (say % 2 == 0)
+            //        {
+            //            eded++;
+            //        }
+            //        else
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Red;
+            //            Console.WriteLine("Cut eded deyil");
+            //            Console.ResetColor();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Bos ola bilmezz");
+            //    }
+
+            //}
+            //Console.WriteLine("Toplam Cut ededlerin sayi:");
+            //Console.WriteLine(eded);
+
+            //*************************
+
+
+            //int say1;
+            //int say2;
+            //int toplam;
+
+            //say1 = Convert.ToInt32(Console.ReadLine());
+            //say2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (say1 > say2)
+            //{
+            //    toplam = say1 * say1;
+            //    Console.WriteLine(toplam);
+            //}
+            //else
+            //{
+            //    toplam = say2 * say2;
+            //    Console.WriteLine(toplam);
+
+            //} ***********************************
+
+            //float benzin; ***************************
+            //float km;
+            //float ortalama;
+
+            //Console.WriteLine("Ne qeder benzin doldurmusuz ?");
+            //benzin = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Ne qeder yol getmisiz ?");
+            //km = Convert.ToInt32(Console.ReadLine());
+            //ortalama = benzin/km;
+            //Console.WriteLine(ortalama); ************************
+
+            //int say=0; * ****************
+            //int toplam = 0;
+            //for (say = 1; say <= 100; say++)
+            //{
+            //    toplam += say;
+            //}
+            //Console.WriteLine(toplam); ************
+
+            //int say; *************
+            //for (say = 1; say <= 100; say++)
+            //{
+            //    Console.WriteLine(say);
+            //} ******************
+
+
+            //int say; ***********************
+            //int boyuk = 0;
+            //int kicik = 0;
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    say= Convert.ToInt32(Console.ReadLine());
+            //    if (i == 1)
+            //    {
+            //        boyuk = say;
+            //        kicik = say ;
+            //    }
+            //    if (kicik > say)
+            //    {
+            //         kicik=say;
+            //    }
+            //    else if(boyuk<say)
+            //    {
+            //        boyuk=say;
+            //    }   
+            //    Console.WriteLine("eN BOYUk say" + boyuk);
+            //    Console.WriteLine("eN kicik say" + kicik);
+            //} ****************
+
+            //double maas, zam, artanmaas = 0;********************
+            //Console.WriteLine("Maasiniz yazin");
+            //maas=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Nece faiz artim gelib maasiniza?");
+            //zam = Convert.ToDouble(Console.ReadLine());
+            //artanmaas = maas * zam / 100 +maas;
+            //Console.WriteLine("Artan maasiniz");
+            //Console.WriteLine(artanmaas);****************
+
+            //int faktorial=1;*************************
+            //int n;
+            //n=Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    faktorial = i * faktorial;
+            //}
+
+            //Console.WriteLine("Faktorial:" + faktorial) ;*****************
+
+            int a, b, c; 
+
+            for (a = 1; a < 9; a++)
+            {
+                for (b = 0; b < 9; b++)
+                {
+                    for (c = 0; c < 9; c++)
+                    {
+                        if (a + b + c == 3)
+                        {
+                            Console.WriteLine(100 * a + 10 * b + c);
+                        }
+                    }
+                }
+            }
+
+           Console.ReadLine();
+        } 
+
+        public static int test25(string str)
+        {
+            int _a = 0;
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                if(str.Substring(i,2)=="aa")
+                _a++;
+            }
+
+            return _a;
+        }
+
+        public static string test24(string str,int n)
+        {
+            var result = string.Empty;
+            var frontofString = 3;
+
+            if(frontofString > str.Length)
+            {
+                frontofString = str.Length;
+            }
+
+            var front = str.Substring(0, frontofString);
+            for (int i = 0; i < n; i++)
+            {
+                result += front;
+            }
+
+            return result;
+        }
+        public static string test23(string str,int n)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                result = result +str.ToUpper();
+            }
+            return result;
+        }
+        public static string test22(string str)
+        {
+            return str.Length < 3 ? str.ToUpper() : str.Remove(str.Length - 3)
+                + str.Substring(str.Length - 3).ToUpper();
+        }
+
+        public static bool test21(int x, int y)
+        {
+            return Math.Abs(x % 10) == Math.Abs(y % 10);
+        }
+        public static bool test20(string str)
+        {
+            int ctr = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'z')
+                {
+                    ctr++;
+                }
+            }
+            return ctr > 1 && ctr < 4;
+        }
+
+        public static int test19(int x, int y)
+        {
+            if (x >= 20 && x <= 30 || y >= 20 && y <= 30)
+            {
+                var max = Math.Max(x, y);
+                return max;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static bool test18(int x, int y)
+        {
+            return x >= 40 && x <= 50 || y >= 50 && y <= 60;
+        }
+        public static int test17(int x, int y)
+        {
+            const int n = 100;
+            int val1 = Math.Abs(n - x);
+            int val2 = Math.Abs(n - y);
+
+            return val1 == val2 ? 0 : (val1 < val2 ? x : y);
+        }
+
+        public static int test16(int a, int b, int c)
+        {
+            var max = Math.Max(a, Math.Max(b, c));
+            return max;
+        }
+
+        public static string test15(string str)
+        {
+            return str.Substring(1, 2) == "yt" ? str.Remove(1, 2) : str;
+        }
+        public static bool test14(int x, int y, int z)
+        {
+            return (x >= 20 && x <= 50) || (y >= 20 && y <= 50) || (z >= 20 && z <= 50);
+        }
+
+        public static bool test13(int x, int y)
+        {
+            return x >= 100 && y <= 200 || y >= 100 && x <= 200;
+        }
+
+        public static bool test12(int x, int y)
+        {
+            return x < 0 && y > 100 || y < 0 && x > 100;
+        }
+
+        public static bool test11(string str)
+        {
+            return str.StartsWith("C#");
+        }
+
+
+
+        public static bool test10(int x)
+        {
+            return x % 3 == 0 || x % 7 == 0;
+        }
+
+        public static string test9(string str)
+        {
+            var s = str.Substring(str.Length - 1);
+            return s + str + s;
+        }
+
+
+        public static string test8(string str)
+        {
+            if (str.Length < 2)
+            {
+                return str;
+            }
+            else
+            {
+                return str.Substring(0, 2) + str.Substring(0, 2)
+                + str.Substring(0, 2) + str.Substring(0, 2);
+            }
+        }
+
+
+
+        public static string test7(string str)
+        {
+            return str.Length > 1
+                ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1) : str;
+        }
+        public static string test6(string s, int a)
+        {
+            return s.Remove(a, 1);
+        }
+        public static string test5(string s)
+        {
+            if (s.Length > 2 && s.Substring(0, 2) == "if")
+            {
+                return s;
+            }
+            return "if " + s;
+        }
+        public static bool test4(int x)
+        {
+            return Math.Abs(x - 100) <= 10 || Math.Abs(x - 200) <= 10;
+        }
+
+        public static bool test3(int x, int y)
+        {
+            return x + y == 30 || x == 30 || y == 30;
+        }
+
+        public static int test2(int n)
+        {
+            const int x = 51;
+            //return n > x ? (n - x) * 3 : x - n;
+            if (n > x)
+            {
+                return (n - x) * 3;
+            }
+            else
+            {
+                return x - n;
+            }
+        }
+
+        public static int test1(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+
+
+    }
+}
